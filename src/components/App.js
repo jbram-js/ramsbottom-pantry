b539788c84dc1b8485dcabe92afec3db4a6d4b4e
“Joe <josephbramwell@bolstersystems.com>
2020-11-25 21:03:13 +0000

    added wording to what we need page

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import NavBar from "./NavBar";
 import About from "./About";
 import Contact from "./Request.js";
 import Request from "./Contact.js";
+import WhatWeNeed from "./WhatWeNeed.js";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <About />
+      <WhatWeNeed />
       <Request />
       <Contact />
     </div>
---
+++ src/components/WhatWeNeed.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const WhatWeNeed = () => {
+  return (
+    <div className="WhatWeNeed">
+      <h1>What we Need</h1>
+      <p>
+        We rely on the generosity and community spirit of Ramsbottom. We always
+        need non-perishable food items, toiletries & cleaning products.
+      </p>
+      <ul className="wwn-list">
+        <li className="wwn-list-item">Pasta & rice</li>
+        <li className="wwn-list-item">Tins & jars</li>
+        <li className="wwn-list-item">Sauces, jams & spreads</li>
+        <li className="wwn-list-item">Tea, coffee & sugar</li>
+        <li className="wwn-list-item">Cereals</li>
+        <li className="wwn-list-item">Long life milk</li>
+        <li className="wwn-list-item">Squash</li>
+        <li className="wwn-list-item">Crisps</li>
+        <li className="wwn-list-item">Biscuits</li>
+        <li className="wwn-list-item">Washing detergent</li>
+        <li className="wwn-list-item">Household cleaning products</li>
+        <li className="wwn-list-item">
+          toiletries, sanitry products & nappies
+        </li>
+        <li className="wwn-list-item">Pet food</li>
+      </ul>
+    </div>
+  );
+};
+
+export default WhatWeNeed;
